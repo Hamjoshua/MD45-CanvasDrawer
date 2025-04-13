be237d7bcbdc81da395ff5dd08b8d69c96dde928
Hamjoshua <biryukov-10@list.ru>
2025-04-13 13:41:32 +0700

    CanvasDrawer теперь рисует круг

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.md45_canvasdrawer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.md45_canvasdrawer"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/md45_canvasdrawer/CanvasDrawer.kt
+++ app/src/main/java/com/example/md45_canvasdrawer/CanvasDrawer.kt
@@ -2,33 +2,49 @@ package com.example.md45_canvasdrawer
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.ColorInt
+import androidx.annotation.Dimension
 
 class CanvasDrawerView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    @ColorInt
+    private var paintColor = Color.BLACK
+    @Dimension
+    private var paintSize = 20f
+
     private var fingerX : Float = 0f
     private var fingerY : Float = 0f
-    private lateinit var paint : Paint
-    init {
-        paint = Paint(0).apply {
-            color = 0x101010
-        }
+
+    private var paint : Paint = Paint(0).apply {
+        color = paintColor
+        strokeWidth = paintSize
     }
+
+    fun setColor(colorInt: Int){
+        paintColor = colorInt
+    }
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.d("touch", "${event}")
         if(event != null){
             fingerX = event.getX()
             fingerY = event.getY()
-            invalidate()
+            Log.d("touch", "${fingerX} ${fingerY}")
         }
-
+        invalidate()
         return super.onTouchEvent(event)
     }
     override fun onDraw(canvas: Canvas){
         super.onDraw(canvas)
         canvas.apply {
-            canvas.drawPoint(fingerX, fingerY, paint)
+            Log.d("canvas-drawer", "Point on ${fingerX} ${fingerY}")
+            // canvas.drawPoint(fingerX, fingerY, paint)
+            canvas.drawCircle(fingerX, fingerY, paintSize, paint)
         }
     }
 }
